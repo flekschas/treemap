f0a639898fe9b2bcf982f244ac956af44b145643
Fritz Lekschas <service@lekschas.de>
2015-08-10 11:02:18 -0400

    Switched back to live data via Neo4J. Refactored Neo4JD3 service to avoid querying automatically when the service gets invoked.

--- src/app/treeMap/controller.js
+++ src/app/treeMap/controller.js
@@ -61,19 +61,20 @@ function TreeMapCtrl ($element, $q, $, d3, neo4jD3, HEX, D3Colors, settings) {
   this.treeMap.$grandParent = this.$(this.treeMap.grandParent.node());
 
   /* ---------------------------- [START: STATIC] --------------------------- */
-  this.d3.json('../data/cl.json', function(error, data) {
-    if (error) return console.warn(error);
-    this.data = data;
-    this.draw();
-  }.bind(this));
+  // this.d3.json('../data/cl.json', function(error, data) {
+  //   if (error) return console.warn(error);
+  //   this.data = data;
+  //   this.draw();
+  // }.bind(this));
   /* ----------------------------- [END: STATIC] ---------------------------- */
 
   /* ----------------------------- [START: LIVE] ---------------------------- */
-  // neo4jD3.d3
-  //   .then(function (data) {
-  //     this.data = data;
-  //     this.draw();
-  //   }.bind(this));
+  neo4jD3
+    .get()
+    .then(function (data) {
+      this.data = data;
+      this.draw();
+    }.bind(this));
   /* ------------------------------ [END: LIVE] ----------------------------- */
 }
 
